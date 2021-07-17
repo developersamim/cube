20c6151ee96c63bd24d061f7e901f31105aecafb
Samim Ahmed <developersamim@gmail.com>
2021-07-17 18:05:42 +1000

    added projects with docker

---
+++ services/Temperature.Infrastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Temperature.Infrastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
